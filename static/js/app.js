beb5dad3a8757f50cd0f83c1ec6ce0e0742903bf
yuwat <yuwat@mfec.co.th>
2019-09-03 16:57:28 +0700

    hilight keyword

--- static/js/app.js
+++ static/js/app.js
@@ -1,45 +1,73 @@
+function on_success(text, resp){
+  console.log(resp);
+
+  // caterories
+  for(c in resp.classes){
+    $('.table.category tbody').append(`
+      <tr>
+        <td>${c}</td>
+        <td>${(resp.classes[c]*100).toFixed(2)}%</td>
+      </tr>
+    `);
+  }
+
+  // remove duplicate entity name
+  let entities2 = [];
+  let unique_keys = [];
+  resp.entities.forEach((en) => {
+    if(unique_keys.includes(en.name)){ return; }
+    unique_keys.push(en.name);
+    entities2.push(en);
+  });
+
+  // entities
+  entities2.forEach((en) => {
+    $('.table.entity tbody').append(`
+      <tr bgcolor='${_type_colors[en.type]}'>
+        <td>${en.name}</td>
+        <td>${en.type}</td>
+      </tr>
+    `);
+  });
+
+  // hilight paragraph
+  $('p.lead').html(text);
+  hilight_entities($('p.lead')[0], entities2);
+
+  $('#ta_text').attr('disabled', false);
+  $('#btn_run').attr('disabled', false);
+}
+
 // bind button
 $('#btn_run').click(() => {
   $('#ta_text').attr('disabled', true);
   $('#btn_run').attr('disabled', true);
 
+  $('p.lead').html('');
   $('.table.category tbody').html('');
   $('.table.entity tbody').html('');
 
   let text = $('#ta_text').val();
+
   $.ajax({
     url: '/nlp_rock',
     type: 'POST',
     contentType: 'application/json',
     dataType: 'json',
-    data: JSON.stringify({
-      text: text,
-    }),
-    success: (resp) => {
-      console.log(resp);
-
-      // caterories
-      for(c in resp.classes){
-        $('.table.category tbody').append(`
-          <tr>
-            <td>${c}</td>
-            <td>${(resp.classes[c]*100).toFixed(2)}%</td>
-          </tr>
-        `);
-      }
-
-      // entities
-      resp.entities.forEach((en) => {
-        $('.table.entity tbody').append(`
-          <tr>
-            <td>${en.name}</td>
-            <td>${en.type}</td>
-          </tr>
-        `);
-      });
-
-      $('#ta_text').attr('disabled', false);
-      $('#btn_run').attr('disabled', false);
-    }
+    data: JSON.stringify({ text: text }),
+    success: function(resp){
+      on_success(text, resp);
+    },
   });
 });
+
+// set default text
+let text = "สุดยอดมือถือเรือธงสเปคจัดเต็มอย่าง OnePlus 7 Pro ที่เข้ามาวางจำหน่ายในบ้านเราเมื่อช่วงปลายเดือนพฤษภาคมที่ผ่านมา นับว่าได้รับความนิยมจากเหล่าแฟนๆ OnePlus ในบ้านเราอยู่ไม่น้อย ไม่ว่าจะเป็นสเปคสุดแรง, หน้าจอ 90Hz สุดงาม, หน่วยความจำแบบ UFS 3.0, กล้องระดับท็อป ฯลฯ ซึ่งล่าสุดได้มีการปรับราคาของ OnePlus 7 Pro ลงอีกรุ่นละ 2,000 บาท ให้หลายๆ คนได้ตัดสินใจถอยมาเป็นเจ้าของกันง่ายขึ้นอีก";
+$('#ta_text').val(text);
+
+// render default result
+let s = `
+{"classes":{"/Computers & Electronics/Consumer Electronics":0.9800000190734863,"/Internet & Telecom/Mobile & Wireless/Mobile Phones":0.9700000286102295},"entities":[{"_name":"OnePlus 7 Pro","name":"OnePlus 7 Pro","type":"CONSUMER_GOOD"},{"_name":"country","name":"ประเทศ","type":"LOCATION"},{"_name":"specification","name":"สเปค","type":"OTHER"},{"_name":"fans","name":"แฟน ๆ","type":"PERSON"},{"_name":"memory","name":"หน่วยความจำ","type":"OTHER"},{"_name":"screen","name":"จอภาพ","type":"OTHER"},{"_name":"OnePlus","name":"OnePlus","type":"ORGANIZATION"},{"_name":"price","name":"ราคา","type":"OTHER"},{"_name":"version","name":"รุ่น","type":"OTHER"},{"_name":"UFS 3.0","name":"UFS 3.0","type":"ORGANIZATION"},{"_name":"owner","name":"เจ้าของ","type":"PERSON"},{"_name":"people","name":"คน","type":"PERSON"},{"_name":"camera","name":"กล้อง","type":"CONSUMER_GOOD"},{"_name":"2,000 baht","name":"2,000 บาท","type":"PRICE"},{"_name":"90","name":"90","type":"NUMBER"},{"_name":"7","name":"7","type":"NUMBER"},{"_name":"3.0","name":"3.0","type":"NUMBER"},{"_name":"2,000","name":"2,000","type":"NUMBER"},{"_name":"7","name":"7","type":"NUMBER"}]}
+`;
+let resp = JSON.parse(s);
+on_success(text, resp);
---
+++ static/js/hilight.js
@@ -0,0 +1,34 @@
+/* https://github.com/googleapis/google-cloud-python/blob/master/language/google/cloud/language_v1/gapic/enums.py */
+let _type_colors = {
+  UNKNOWN:        '#cb91ec',
+  PERSON:         '#e2cc8c',
+  LOCATION:       '#be9058',
+  ORGANIZATION:   '#eff066',
+  EVENT:          '#8ede83',
+  WORK_OF_ART:    '#bed4e4',
+  CONSUMER_GOOD:  '#b5e076',
+  OTHER:          '#FAE8F0',
+  PHONE_NUMBER:   '#f69dc5',
+  ADDRESS:        '#a986e6',
+  DATE:           '#ddc75d',
+  NUMBER:         '#f9a951',
+  PRICE:          '#e0ab8a',
+};
+
+function hilight_entities(dom, entities){
+  let html = $(dom).html();
+
+  // mark hilight by btoa
+  entities.forEach((en, i) => {
+    html = html.replace(RegExp(en.name, 'i'), `___${btoa(i)}___`);
+  });
+
+  // update text
+  entities.forEach((en, i) => {
+    html = html.replace(RegExp(`___${btoa(i)}___`, 'i'),
+            `<span style='background-color: ${_type_colors[en.type]};'>${en.name}</span>`);
+  });
+
+  // render
+  $(dom).html(html);
+}
